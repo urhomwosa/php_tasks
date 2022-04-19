d2b3ecb64ce0060b24863f94e70e0ae9ee35bb94
urhomwosa <95337784+urhomwosa@users.noreply.github.com>
2022-04-19 23:30:43 +0300

    Add files via upload

--- core/database.php
+++ core/database.php
@@ -1,1 +1,35 @@
+<?php
 
+
+namespace App\Core;
+
+abstract class Database
+{
+
+    private $conn;
+
+    public function __construct()
+    {
+        # code...
+        $dbServername = "127.0.0.1";
+        $dbUsername = "root";
+        $dbPassword = "";
+        $dbName = "scandiweb";
+
+
+        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+        $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+        if (!$conn) {
+            echo 'connection error: ' . mysqli_connect_error();
+        }
+
+        $this->conn = $conn;
+    }
+
+    protected function getConnection()
+    {
+        return $this->conn;
+    }
+}
