ea123826de247a19f09a0fe0e537b5b5fcd9fcde
urhomwosa <95337784+urhomwosa@users.noreply.github.com>
2022-04-19 23:32:42 +0300

    Add files via upload

--- model/Book.php
+++ model/Book.php
@@ -1,1 +1,13 @@
+<?php
 
+namespace App\Model;
+
+require_once __DIR__ . '/product.php';
+
+class Book extends Product
+{
+    public function getAttribute()
+    {
+        return  "Weight: $this->attribute KG";
+    }
+}
